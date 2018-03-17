7a4e34561d74e66d3e2f462ec51c3d54f079e186
Кирилл <Admin@blockapp.org>
2018-03-17 20:14:52 +0300

    Packet Enums

---
+++ BlockAPP Core/Core/Network/Enums/PacketType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlockAPP_Core.Core.Network.Enums
+{
+    public enum PacketType
+    {
+        TYPE_RequestCredentials = 0,
+        TYPE_MyCredentials = 1,
+        TYPE_Registered = 2,
+        TYPE_HostExiting = 3,
+        TYPE_ClientData = 4,
+        TYPE_ClientDisconnecting = 5,
+        TYPE_CredentialsUpdate = 6,
+        TYPE_Close = 7,
+        TYPE_Message = 8,
+        TYPE_MessageReceived = 9
+    }
+}
---
+++ BlockAPP Core/Core/Network/Enums/PacketTypeSubmessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlockAPP_Core.Core.Network.Enums
+{
+    public enum PacketTypeSubmessage
+    {
+        SUBMSG_MessageStart,
+        SUBMSG_MessageBody,
+        SUBMSG_MessageEnd
+    }
+}
